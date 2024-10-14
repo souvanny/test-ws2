d8dfbcfffba72ed5e7af77e6dde19e6aebccb349
souvanny <ssisengrath@hotmail.com>
2024-10-14 04:06:01 +0200

    correction

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends AbstractController
     		$product = new Classes\Product($db, $id);
 
     		if (!empty($product)) {
-    			echo $this->render('product/detail_list.tpl', ['product' => $product]);
+    			echo $this->render('product/detail.tpl', ['product' => $product]);
     		} else {
     			$this->_redirect('product_list.php');
     		}
